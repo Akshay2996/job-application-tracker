4977a6c7901ee01d451d73fcfd4c0b253577f3ca
Akshay Sharma <akshaysharmaay@gmail.com>
2026-01-25 23:50:45 +0530

    Added more styling for the page

--- app/page.tsx
+++ app/page.tsx
@@ -6,13 +6,17 @@ export default function Home() {
       <main className="flex-1">
         {/* Hero section */}
         <section className="container mx-auto py-32 px-4">
-          <div>
-            <h1 className="text-black">
-              A better way to track job applications.
+          <div className="mx-auto max-w-4xl text-center">
+            <h1 className="text-black mb-6 text-6xl font-bold">
+              A better way to track job applications
             </h1>
-            <p className="text-muted-foreground">
+            <p className="text-muted-foreground mb-10 text-xl">
               Capture, organize and manage your job search in one place.
             </p>
+            <div className="flex flex-col items-center gap-4">
+              <button>Start for free</button>
+              <p>Free forever. No credit card required.</p>
+            </div>
           </div>
         </section>
       </main>
