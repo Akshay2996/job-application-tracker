461c0b07e1cda218cb6a4b688860fc06be5fcda0
Akshay Sharma <akshaysharmaay@gmail.com>
2026-01-27 00:00:25 +0530

    Working on tabs to show hero images on the landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import { ArrowRight } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
+
+type TabTypes = "organize" | "hired" | "boards";
 
 export default function Home() {
+  const [activeTab, setActiveTab] = useState<TabTypes>("organize");
+
   return (
     <div className="flex flex-col bg-white min-h-screen">
       <main className="flex-1">
@@ -28,6 +35,61 @@ export default function Home() {
               <p className="text-sm text-muted-foreground">
                 Free forever. No credit card required.
               </p>
+            </div>
+          </div>
+        </section>
+        {/* Hero images section */}
+        <section className="border-t bg-white py-16">
+          <div className="container mx-auto px-4">
+            <div className="mx-auto max-w-6xl">
+              {/* Tabs */}
+              <div className="flex gap-2 justify-center mb-8">
+                <Button
+                  className="cursor-pointer"
+                  onClick={() => setActiveTab("organize")}
+                >
+                  Organize Applications
+                </Button>
+                <Button
+                  className="cursor-pointer"
+                  onClick={() => setActiveTab("hired")}
+                >
+                  Get Hired 
+                </Button>
+                <Button
+                  className="cursor-pointer"
+                  onClick={() => setActiveTab("boards")}
+                >
+                  Manage Boards
+                </Button>
+              </div>
+              <div className="relative mx-auto max-w-5xl overflow-hidden rounded-lg border border-gray-200 shadow-xl">
+                {/* Images */}
+                {activeTab === "organize" && (
+                  <Image
+                    src="/hero-images/hero1.png"
+                    width={1200}
+                    height={800}
+                    alt="Organize Applications"
+                  />
+                )}
+                {activeTab === "hired" && (
+                  <Image
+                    src="/hero-images/hero2.png"
+                    width={1200}
+                    height={800}
+                    alt="Get Hired"
+                  />
+                )}
+                {activeTab === "boards" && (
+                  <Image
+                    src="/hero-images/hero3.png"
+                    width={1200}
+                    height={800}
+                    alt="Manage Boards"
+                  />
+                )}
+              </div>
             </div>
           </div>
         </section>
