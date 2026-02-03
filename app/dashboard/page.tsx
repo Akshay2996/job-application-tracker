cc60aee09ea636f1701ca3ecbe664a03cdb0afa1
Akshay Sharma <akshaysharmaay@gmail.com>
2026-02-03 23:47:55 +0530

    Populating the kanban board with the deafult column values

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -16,9 +16,9 @@ export default async function Dashboard() {
   const board = await Board.findOne({
     userId: session.user.id,
     name: "Job Hunt",
+  }).populate({
+    path: "columns",
   });
-
-  console.log("Board: ", board);
 
   return (
     <div className="min-h-screen bg-white">
@@ -27,7 +27,10 @@ export default async function Dashboard() {
           <h1 className="text-3xl font-bold text-black">{board.name}</h1>
           <p className="text-gray-600">Track your job applications</p>
         </div>
-        <KanbanBoard board={board} userId={session.user.id} />
+        <KanbanBoard
+          board={JSON.parse(JSON.stringify(board))}
+          userId={session.user.id}
+        />
       </div>
     </div>
   );
--- components/kanban-board.tsx
+++ components/kanban-board.tsx
@@ -1,12 +1,85 @@
 "use client";
 
-import { Board } from "@/lib/models/model.types";
+import { Board, Column } from "@/lib/models/model.types";
+import { Award, Calendar, CheckCircle2, Mic, XCircle } from "lucide-react";
+import { Card, CardHeader, CardTitle } from "./ui/card";
 
 interface KanbanBoardProps {
   board: Board;
   userId: string;
 }
 
+interface ColConfig {
+  color: string;
+  icon: React.ReactNode;
+}
+
+const COLUMN_CONFIG: Array<ColConfig> = [
+  {
+    color: "bg-cyan-500",
+    icon: <Calendar className="h-4 w-4" />,
+  },
+  {
+    color: "bg-purple-500",
+    icon: <CheckCircle2 className="h-4 w-4" />,
+  },
+  {
+    color: "bg-green-500",
+    icon: <Mic className="h-4 w-4" />,
+  },
+  {
+    color: "bg-yellow-500",
+    icon: <Award className="h-4 w-4" />,
+  },
+  {
+    color: "bg-red-500",
+    icon: <XCircle className="h-4 w-4" />,
+  },
+];
+
+interface DroppableColumnProps {
+  column: Column;
+  boardId: string;
+  config: ColConfig;
+}
+
+function DroppableColumn({ column, boardId, config }: DroppableColumnProps) {
+  return (
+    <Card>
+      <CardHeader className={`${config.color}`}>
+        <div>
+          <div>
+            {config.icon}
+            <CardTitle>{column.name}</CardTitle>
+          </div>
+        </div>
+      </CardHeader>
+    </Card>
+  );
+}
+
 export default function KanbanBoard({ board, userId }: KanbanBoardProps) {
-  return <></>;
+  const columns = board.columns;
+  return (
+    <>
+      <div>
+        <div>
+          {columns.map((column, key) => {
+            const config = COLUMN_CONFIG[key] || {
+              color: "bg-gray-500",
+              icon: <Calendar className="h-4 w-4" />,
+            };
+            return (
+              <DroppableColumn
+                key={key}
+                boardId={board._id}
+                config={config}
+                column={column}
+              />
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
 }
