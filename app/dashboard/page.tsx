8799e784766ec6b4e6808a53d9ca65f4bd63441b
Akshay Sharma <akshaysharmaay@gmail.com>
2026-02-03 20:37:53 +0530

    Started working on the Dashboard page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,33 @@
+import { getSession } from "@/lib/auth/auth";
+import connectDB from "@/lib/db";
+import { Board } from "@/lib/models";
+import { redirect } from "next/navigation";
 import React from "react";
 
-export default function Dashboard() {
-  return <div>Dashboard  Page</div>;
+export default async function Dashboard() {
+  const session = await getSession();
+
+  if (!session?.user) {
+    redirect("/sign-in");
+  }
+
+  await connectDB();
+
+  const board = await Board.findOne({
+    userId: session.user.id,
+    name: "Job Hunt",
+  });
+
+  console.log("Board: ", board);
+
+  return (
+    <div className="min-h-screen bg-white">
+      <div className="container mx-auto p-6">
+        <div className="mb-6">
+          <h1 className="text-3xl font-bold text-black">{board.name}</h1>
+          <p className="text-gray-600">Track your job applications</p>
+        </div>
+      </div>
+    </div>
+  );
 }
